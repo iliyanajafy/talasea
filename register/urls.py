c8d7e26b69b57f30b2cccfd370560d230545db49
iliya <iliyanajafydalir@gmail.com>
2024-09-29 22:29:47 +0330

    login page added

--- register/urls.py
+++ register/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path("register/",views.register,name=""),
-    # path("login/",views.login,name="")
+    path("register/",views.register,name=""),
+    path("login/",views.login,name="")
 ]
